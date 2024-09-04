096e5f9517dcec855d94270f38b715ad54c0e2f1
Guilherme Rizzatto <guimarquesrz@gmail.com>
2024-09-04 13:45:16 -0300

    Making the layout - 74%

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,13 +9,24 @@ export class MainComponent {
 
   options:boolean = false;
 
+  changePassShow:boolean = false;
+
   showOptions():void{
       if(this.options == true){
         this.options = false;
+        this.changePassShow = false;
       } else {
         this.options = true;
       }
   }
+
+  changePassEnable():void{
+    if(this.changePassShow == true){
+      this.changePassShow = false;
+    } else {
+      this.changePassShow = true;
+    }
+}
 
 
 
